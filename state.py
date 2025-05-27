e910cbd0acda86a16cd102352ea6a0f269e38371
Aleksa Zatezalo <zabumaphu@gmail.com>
2025-05-28 00:20:16 +0200

    Printing legal moves

--- state.py
+++ state.py
@@ -1,11 +1,22 @@
+#!/usr/bin/env python3
 """
 Author: Aleksa Zatezalo
 Date: May 2025
-Description: Represents a chess board.
+Description: Represents a chess board's state.
 """
 
 import chess
 
-class Board():
+class State():
     def __init__(self):
         self.board = chess.Board()
+
+    def edges(self):
+        return list(self.board.legal_moves)
+
+    def value(self):
+        return 1
+    
+if __name__ == "__main__":
+    s = State()
+    print(s.edges())
