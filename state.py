139e6fd4fce24d2f70f4df7f41cc1e79721d2517
Aleksa Zatezalo <zabumaphu@gmail.com>
2025-05-28 00:25:51 +0200

    Added PGN chess games

--- state.py
+++ state.py
@@ -11,10 +11,15 @@ class State():
     def __init__(self):
         self.board = chess.Board()
 
+    def serialize(self):
+        #257 bits according to readme
+        pass
+
     def edges(self):
         return list(self.board.legal_moves)
 
     def value(self):
+        # TODO: add neural net here
         return 1
     
 if __name__ == "__main__":
