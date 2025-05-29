6d6f2f27f3ed8f75e1dff77cf45f4379f17de7bb
Aleksa Zatezalo <zabumaphu@gmail.com>
2025-05-29 23:28:38 +0200

    Reading PGNs

---
+++ train.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import os 
+import chess.pgn
+
+for fn in os.listdir("data"):
+        pgn = open(os.path.join("data", fn))
+        while 1:
+                try:
+                        game = chess.pgn.read_game(pgn)
+                except Exception:
+                        break
+                
