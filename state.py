b94333cf4e049dbae1afcaf98e6e08091a4dc6e5
Aleksa Zatezalo <zabumaphu@gmail.com>
2025-05-30 00:24:14 +0200

    Serialized

--- state.py
+++ state.py
@@ -16,12 +16,28 @@ class State():
             self.board = board
 
     def serialize(self):
-        state = np.zeros((8, 8, 5))
+        assert self.board.is_valid()
+
+        bstate = np.zeros(64, np.uint8)
+        for i in range(64):
+            pp = self.board.piece_at(i)
+            if pp is not None:
+                print(pp)
+                pass
+        bstate = bstate.reshape(8, 8)
+        exit(0)
+        state = np.zeros((8, 8, 5), np.uint8)
+
+        # 0-3 columns to binary
+        state[:, :, 0] = (bstate>>3)&1
+        state[:, :, 1] = (bstate>>2)&1
+        state[:, :, 2] = (bstate>>1)&1
+        state[:, :, 3] = (bstate>>0)&1
+
+        # 4th column is who's turn it is
         state[:, :, 4] = (self.board.turn*1.0)
-        print(state)
-        #257 bits according to readme
-        pp = self.board.shredder_fen()
-        return pp
+        
+        return state
 
     def edges(self):
         return list(self.board.legal_moves)
