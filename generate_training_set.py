254e16fc2cf0cd6b39139840f8dce4b83b38fb9b
Aleksa Zatezalo <zabumaphu@gmail.com>
2025-06-03 14:26:33 +0200

    Began training data.

--- generate_training_set.py
+++ generate_training_set.py
@@ -4,6 +4,7 @@ import os
 import chess.pgn
 from state import State
 import numpy as np
+import h5py
 
 # pgn files in the data folder
 def get_dataset(num_samples=None):
@@ -17,7 +18,6 @@ def get_dataset(num_samples=None):
                         except Exception:
                                 break
                         print(f"parsing game %d, got %d examples" % (gn, len(X)))
-                        gn += 1
                         board = game.board()
                         value = {'1/2-1/2':0, '0-1':-1, '1-0':1}[game.headers['Result']]
                         for i, move in enumerate(game.mainline_moves()):
@@ -27,5 +27,15 @@ def get_dataset(num_samples=None):
                                 Y.append(value)
                         if num_samples is not None and len(X) > num_samples:
                                 return X, Y
+                        gn += 1
+        X = np.array(X)
+        Y = np.array(Y)
+        return X, Y
+
 if __name__ =="__main__":
-       X, Y = get_dataset(10000)
+       X, Y = get_dataset(1000)
+       np.savez("processed/dataset.npz", X, Y)
+#        h5 = h5py.File('processed/traingme.h5', 'w')
+#        h5.create_dataset('X', X)
+#        h5.create_dataset('X', Y)
+#        h5.close()
---
+++ train.py
@@ -0,0 +1,14 @@
+from torch.utils.data import Dataset
+import numpy as np
+
+class ChessValueDataset(Dataset):
+    def __init__(self):
+        dat = np.loadz("processed/dataset.npz")
+        self.X = dat['X']
+        self.Y = dat['Y']
+
+    def __len__(self):
+        return self.X.shape[0]
+    
+    def __getitem__(self, idx):
+        return{'X': self.X[idx], 'Ý'}
