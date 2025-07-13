e91cf31dc0ff4aa6bb92273c6fc53310d9448607
Aleksa Zatezalo <zabumaphu@gmail.com>
2025-07-13 22:00:29 +0200

    Should work?

--- generate_training_set.py
+++ generate_training_set.py
@@ -6,42 +6,41 @@ Description: Opens and extracts chess games from .npz archive
 Date: July 2025
 """
 
-import os 
+#!/usr/bin/env python3
+import os
 import chess.pgn
-from state import State
 import numpy as np
-import h5py
+from state import State
 
-# pgn files in the data folder
 def get_dataset(num_samples=None):
-        X, Y = [], []
-        gn = 0
-        for fn in os.listdir("data"):
-                pgn = open(os.path.join("data", fn))
-                while 1:
-                        try:
-                                game = chess.pgn.read_game(pgn)
-                        except Exception:
-                                break
-                        print(f"parsing game %d, got %d examples" % (gn, len(X)))
-                        board = game.board()
-                        value = {'1/2-1/2':0, '0-1':-1, '1-0':1}[game.headers['Result']]
-                        for i, move in enumerate(game.mainline_moves()):
-                                board.push(move)
-                                ser =  State(board).serialize()[:, :, 0]
-                                X.append(ser)
-                                Y.append(value)
-                        if num_samples is not None and len(X) > num_samples:
-                                return X, Y
-                        gn += 1
-        X = np.array(X)
-        Y = np.array(Y)
-        return X, Y
+  X,Y = [], []
+  gn = 0
+  values = {'1/2-1/2':0, '0-1':-1, '1-0':1}
+  # pgn files in the data folder
+  for fn in os.listdir("data"):
+    pgn = open(os.path.join("data", fn))
+    while 1:
+      game = chess.pgn.read_game(pgn)
+      if game is None:
+        break
+      res = game.headers['Result']
+      if res not in values:
+        continue
+      value = values[res]
+      board = game.board()
+      for i, move in enumerate(game.mainline_moves()):
+        board.push(move)
+        ser = State(board).serialize()
+        X.append(ser)
+        Y.append(value)
+      print("parsing game %d, got %d examples" % (gn, len(X)))
+      if num_samples is not None and len(X) > num_samples:
+        return X,Y
+      gn += 1
+  X = np.array(X)
+  Y = np.array(Y)
+  return X,Y
 
-if __name__ =="__main__":
-       X, Y = get_dataset()
-       np.savez("processed/dataset.npz", X, Y)
-#        h5 = h5py.File('processed/traingme.h5', 'w')
-#        h5.create_dataset('X', X)
-#        h5.create_dataset('X', Y)
-#        h5.close()
+if __name__ == "__main__":
+  X,Y = get_dataset(25000000)
+  np.savez("processed/dataset_25M.npz", X, Y)
--- train.py
+++ train.py
@@ -16,39 +16,60 @@ class ChessValueDataset(Dataset):
         dat = np.load("processed/dataset.npz")
         self.X = dat['arr_0']
         self.Y = dat['arr_1']
-        print("loaded", self.X.shape, self.Y.shape)
+        print(self.X.shape, self.Y.shape)
 
     def __len__(self):
         return self.X.shape[0]
     
     def __getitem__(self, idx):
-        return{'X': self.X[idx], 'Y':self.Y[idx]}
+        return{self.X[idx], self.Y[idx]}
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
+        self.a1 = F.relu(nn.Conv2d(5, 16, kernel_size=3))
+        self.a2 = F.relu(nn.Conv2d(16, 16, kernel_size=3))
+        self.a3 = F.relu(nn.Conv2d(16, 32, kernel_size=3))
+
+        self.b1 = F.relu(nn.Conv2d(32, 32, kernel_size=3))
+        self.b2 = F.relu(nn.Conv2d(32, 32, kernel_size=3))
+        self.b3 = F.relu(nn.Conv2d(32, 64, kernel_size=3))
+
+        self.c1 = F.relu(nn.Conv2d(64, 64, kernel_size=3))
+        self.c2 = F.relu(nn.Conv2d(64, 64, kernel_size=3))
+        self.c3 = F.relu(nn.Conv2d(64, 128, kernel_size=3))
+
+        self.d1 = F.relu(nn.Conv2d(128, 128, kernel_size=3))
+        self.d2 = F.relu(nn.Conv2d(128, 128, kernel_size=3))
+        self.d3 = F.relu(nn.Conv2d(128, 128, kernel_size=3))
+
+        self.lost = nn.Linear(128, 1)
 
     def forward(self, x):
-        x = F.relu(nn.Conv2d(16, 16, kernel_size=3))
-        x = F.relu(nn.Conv2d(16, 16, kernel_size=3))
-        x = F.relu(nn.Conv2d(16, 31, kernel_size=3))
-        x = F.max_pool2d(x)
+        x = F.relu(self.a1(x))
+        x = F.relu(self.a2(x))
+        x = F.relu(self.a3(x))
+        x = F.max_pool2d(x, 2)
 
         # 4x4
-        x = F.relu(nn.Conv2d(32, 32, kernel_size=3))
-        x = F.relu(nn.Conv2d(32, 32, kernel_size=3))
-        x = F.relu(nn.Conv2d(32, 64, kernel_size=3))
+        x = F.relu(self.b1)
+        x = F.relu(self.b2)
+        x = F.relu(self.b3)
         x = F.max_pool2d(x)
 
         # 2x2
-        x = F.relu(nn.Conv2d(64, 64, kernel_size=3))
-        x = F.relu(nn.Conv2d(64, 64, kernel_size=3))
-        x = F.relu(nn.Conv2d(64, 64, kernel_size=3))
+        x = F.relu(self.c1)
+        x = F.relu(self.c2)
+        x = F.relu(self.c3)
         x = F.max_pool2d(x)
 
-        # 1x64
-        x = x.view(-1, 64)
-        x = nn.Linear(64, 1)(x)
+        # 1x128
+        x = F.relu(self.d1)
+        x = F.relu(self.d2)
+        x = F.relu(self.d3)
+
+        x = x.view(-1, 128)
+        x = self.last(x)
 
         # value output
         return F.log_softmax(x)
@@ -56,3 +77,17 @@ class Net(nn.Module):
 chess_dataset = ChessValueDataset()
 model = Net()
 optimizer = optim.Adam(model.parameters())
+
+device = "cpu"
+
+model.train()
+for batch_idx, (data, target) in enumerate(train_loader):
+    data = data.unsqueeze_(-1)
+    print(data.shape)
+    print(target.shape)
+    data, target = data.to(device), target.to(device)
+    optimizer.zero_grad()
+    output = model(data)
+    loss = F.all_loss(output, target)
+    loss.backward()
+    optimizer.step()
