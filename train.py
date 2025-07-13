dd72cd5554102f1820ddfb5ca08bc52000c624e3
Aleksa Zatezalo <zabumaphu@gmail.com>
2025-07-13 21:13:10 +0200

    Optimizer created

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ Date: July 2025
 
 from torch.utils.data import Dataset
 import numpy as np
-import torch
+import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -23,33 +23,36 @@ class ChessValueDataset(Dataset):
     
     def __getitem__(self, idx):
         return{'X': self.X[idx], 'Y':self.Y[idx]}
-    
-chess_dataset = ChessValueDataset()
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
     def forward(self, x):
-        x = F.relu(nn.Conv2d(5, 10, kernel_size=3))
-        x = F.relu(nn.Conv2d(10, 10, kernel_size=3))
-        x = F.relu(nn.Conv2d(10, 10, kernel_size=3))
+        x = F.relu(nn.Conv2d(16, 16, kernel_size=3))
+        x = F.relu(nn.Conv2d(16, 16, kernel_size=3))
+        x = F.relu(nn.Conv2d(16, 31, kernel_size=3))
         x = F.max_pool2d(x)
 
         # 4x4
-        x = F.relu(nn.Conv2d(20, 20, kernel_size=3))
-        x = F.relu(nn.Conv2d(20, 20, kernel_size=3))
-        x = F.relu(nn.Conv2d(20, 20, kernel_size=3))
+        x = F.relu(nn.Conv2d(32, 32, kernel_size=3))
+        x = F.relu(nn.Conv2d(32, 32, kernel_size=3))
+        x = F.relu(nn.Conv2d(32, 64, kernel_size=3))
         x = F.max_pool2d(x)
 
         # 2x2
-        x = F.relu(nn.Conv2d(40, 40, kernel_size=3))
-        x = F.relu(nn.Conv2d(40, 40, kernel_size=3))
-        x = F.relu(nn.Conv2d(40, 40, kernel_size=3))
+        x = F.relu(nn.Conv2d(64, 64, kernel_size=3))
+        x = F.relu(nn.Conv2d(64, 64, kernel_size=3))
+        x = F.relu(nn.Conv2d(64, 64, kernel_size=3))
         x = F.max_pool2d(x)
 
-        # 1x1
-        x = x.view(-1, 1)
+        # 1x64
+        x = x.view(-1, 64)
+        x = nn.Linear(64, 1)(x)
 
-        return F.log_softmax(x, dim=1)
+        # value output
+        return F.log_softmax(x)
 
+chess_dataset = ChessValueDataset()
+model = Net()
+optimizer = optim.Adam(model.parameters())
