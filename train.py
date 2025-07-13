347adaba0c5705be408de1adef560e9f3e89f044
Aleksa Zatezalo <zabumaphu@gmail.com>
2025-07-13 21:06:06 +0200

    Removed code portion

--- train.py
+++ train.py
@@ -51,11 +51,5 @@ class Net(nn.Module):
         # 1x1
         x = x.view(-1, 1)
 
-        x = F.relu(F.max_pool2d(self.conv1(x)), 2)
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fe1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
